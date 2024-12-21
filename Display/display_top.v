4f34cd182b2c3126b61afb8e89124539bcb907cd
BethTsai <146188828+BethTsai@users.noreply.github.com>
2024-12-21 21:48:58 +0800

    display_block not done

--- Display/display_top.v
+++ Display/display_top.v
@@ -17,6 +17,7 @@ module Display_top (
     output wire [6:0] DISPLAY,
     output wire [3:0] DIGIT
 );
+	localparam FRAME = 12'h732;
 	wire clk_25MHz;
     clock_divider #(.n(2)) m2 (.clk(clk), .clk_div(clk_25MHz));
 
@@ -26,9 +27,9 @@ module Display_top (
 	reg [11:0] pixel;
 	reg [11:0] pixel_bricks;
 	wire [11:0] pixel_window;
-	reg [64*64-1:0] pixel_nums, pixel_circle;
+	reg [0:1024-1] pixel_circle;
 	reg [14:0] pixel_addr;
-	wire draw_window, draw_frame, draw_circle, draw_line, draw_nums;
+	wire draw_window, draw_circle, draw_line, draw_nums, draw_outer_frame;
 	
 	wire [3:0] one_num;
 	wire [15:0] nums;
@@ -57,7 +58,7 @@ module Display_top (
 		.block_y(block_y),
 		.pixel_x(pixel_x),
 		.pixel_y(pixel_y),
-		.block_pixel(pixel)
+		.pixel_window(pixel_window)
 	);
 
 	SevenSegment Sevenseg_inst0(
@@ -70,9 +71,40 @@ module Display_top (
 	
 	// Store bricks_wall
 	blk_mem_gen_0 blk_mem_gen_0_inst( .clka(clk_25MHz), .dina(dina), .wea(0), .addra(pixel_addr), .douta(pixel_bricks));
+	// Store window
+	blk_mem_gen_1 blk_mem_gen_1_inst( .clka(clk_25MHz), .dina(dina), .wea(0), .addra(pixel_addr), .douta(pixel_window));
 	// Store circles
-	blk_mem_gen_3 blk_mem_gen_3_inst( .clka(clk_25MHz), .dina(dina), .wea(0), .addra(pixel_addr), .douta(pixel_circle));
+	blk_mem_gen_3 blk_mem_gen_3_inst( .clka(clk_25MHz), .dina(dina), .wea(0), .addra(0), .douta(pixel_circle));
 
 	assign draw_window = (h_cnt >= 160 && h_cnt < 480) && (v_cnt >= 80 && v_cnt < 400);
+	assign draw_outer_frame = (((h_cnt >= 158 && h_cnt < 160) || (h_cnt >= 480 && h_cnt < 482)) && (v_cnt >= 78 && v_cnt < 402)) ||
+								(((v_cnt >= 78 && v_cnt < 80) || (v_cnt >= 400 && v_cnt < 402)) && (h_cnt >= 158 && h_cnt < 482));
 
+	always @(*) begin
+		if(draw_window) begin
+			pixel_addr = (h_cnt - 160) + (v_cnt - 80) * 320;
+		end
+		else begin
+			pixel_addr = (v_cnt%120)*160 + h_cnt%160;
+		end
+	end
+
+	always @(*) begin
+		if(draw_line) begin
+		end
+		else if(draw_window) begin
+			if(draw_circle) begin
+				pixel = pixel_circle[pixel_x + pixel_y * 64] == 0 ? 12'h0 : pixel_window;
+			end
+			else begin
+				pixel = pixel_window;
+			end
+		end
+		else if(draw_outer_frame)begin
+			pixel = FRAME;
+		end
+		else begin
+			pixel = pixel_bricks;
+		end
+	end
 endmodule
