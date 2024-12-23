c28963226e5072dca4fba860af525f03cccef1dd
BethTsai <146188828+BethTsai@users.noreply.github.com>
2024-12-24 04:15:15 +0800

    update slave guess IO

--- Game/game_top_slave.v
+++ Game/game_top_slave.v
@@ -193,6 +193,9 @@ module Game_Slave(
         .rst(rst),
         .interboard_rst(interboard_rst),
 
+		.interboard_en(interboard_en),
+		.interboard_msg_type(interboard_msg_type),
+		.interboard_number(interboard_number),
         .cur_game_state(cur_state),
         .clear_guess(clear_guess),
         .start_guess(start_guess),
